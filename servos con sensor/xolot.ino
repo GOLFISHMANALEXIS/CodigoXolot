13d7e0f1df6970f6da561b47e776fe2d2143ca5b
Alexis Villegas <92242938+GOLFISHMANALEXIS@users.noreply.github.com>
2023-10-12 22:01:08 -0600

    Update xolot.ino

--- servos con sensor/xolot.ino
+++ servos con sensor/xolot.ino
@@ -1,55 +1,67 @@
- #include <Servo.h>
+#include <Servo.h>
 
 Servo servo1;
 Servo servo2;
 Servo servo3;
 Servo servo4;
 
-const int trigPin = 9;     // Pin de salida del sensor ultrasónico
-const int echoPin = 10;    // Pin de entrada del sensor ultrasónico
-const int buttonPin = 8;   // Pin para el botón
+const int trigPin1 = 9;  // Pin de salida del primer sensor ultrasónico
+const int echoPin1 = 10; // Pin de entrada del primer sensor ultrasónico
+const int trigPin2 = 6;  // Pin de salida del segundo sensor ultrasónico
+const int echoPin2 = 7;  // Pin de entrada del segundo sensor ultrasónico
+const int buttonPin = 8;
 
-boolean servosInverted = false; // Indica si los servos están invertidos
+boolean servosInverted = false;
 
 void setup() {
   Serial.begin(9600);
-  servo1.attach(2); // Pin de control del servo 1
-  servo2.attach(3); // Pin de control del servo 2
-  servo3.attach(4); // Pin de control del servo 3
-  servo4.attach(5); // Pin de control del servo 4
+  servo1.attach(2);
+  servo2.attach(3);
+  servo3.attach(4);
+  servo4.attach(5);
 
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT); 
-  pinMode(buttonPin, INPUT_PULLUP); // Configurar el pin del botón con resistencia pull-up
-  
-  servo1.write(0); // Inicialmente, todos los servos en 0°
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  servo1.write(0);
   servo2.write(0);
   servo3.write(0);
   servo4.write(0);
 }
 
 void loop() {
-  // Medir la distancia utilizando el sensor ultrasónico
-  long duration, distance;
-  digitalWrite(trigPin, LOW);
+  // Medir la distancia utilizando el primer sensor ultrasónico
+  long duration1, distance1;
+  digitalWrite(trigPin1, LOW);
   delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(trigPin1, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = duration * 0.034 / 2;
+  digitalWrite(trigPin1, LOW);
+  duration1 = pulseIn(echoPin1, HIGH);
+  distance1 = duration1 * 0.034 / 2;
+
+  // Medir la distancia utilizando el segundo sensor ultrasónico
+  long duration2, distance2;
+  digitalWrite(trigPin2, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW);
+  duration2 = pulseIn(echoPin2, HIGH);
+  distance2 = duration2 * 0.034 / 2;
 
   // Verificar si el botón ha sido presionado
   if (digitalRead(buttonPin) == LOW) {
-    // Mover todos los servos a la posición de 0 grados
     servo1.write(0);
     servo2.write(0);
     servo3.write(0);
     servo4.write(0);
-    delay(500); // Esperar un poco después de mover los servos
+    delay(500);
   } else {
-    // Cambiar la posición de los servos intercaladamente con inversiones en el giro
-    if (distance <= 10) {
+    if (distance1 <= 10 || distance2 <= 10) {
       if (servosInverted) {
         servo1.write(0);
         servo2.write(180);
@@ -61,13 +73,14 @@ void loop() {
         servo3.write(180);
         servo4.write(0);
       }
-      servosInverted = !servosInverted; // Cambiar el estado de inversión
-      
+      servosInverted = !servosInverted;
+
       // Imprimir la distancia en el monitor serie
-      Serial.print("Distancia: ");
-      Serial.println(distance);
-      
-      // Esperar un poco antes de realizar la siguiente medición
+      Serial.print("Distancia 1: ");
+      Serial.print(distance1);
+      Serial.print(" | Distancia 2: ");
+      Serial.println(distance2);
+
       delay(500);
     }
   }
